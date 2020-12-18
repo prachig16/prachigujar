56696bb75070f6951641be3a97bc43b6498f2a06
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-12-17 23:14:33 -0500

    added animation for scroll down and up| changed sliding menu styling| added social media nav

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-  $(document).ready(function() {
+$(document).ready(function() {
 
-    //   $(document).on('click', 'a[href^="#"]', function (e) {
-    //       e.preventDefault();
-    //       $('html, body').stop().animate({
-    //           scrollTop: $($(this).attr('href')).offset().top
-    //       }, 700, 'linear');
-    //   });
+    // declaring a function to change top positiong for the sliding menu in mobile
+    const slidingBar = function (slideTop) {
+        console.log('function');
+        $('.slidingMenu').css({
+            'top': slideTop
+        })
+    }    
+
+    // calling the sliding bar function when clicked on the icon to slide down
+    $('.hamburger').on('click', function(){
+        slidingBar(0);
+    })
+
+    // calling the sliding bar function to slide up the menu when an option is selected
+    $('.slidingMenu').on('click', function(){
+        slidingBar('-100%')
+    })
 
 
-      $(window).scroll(function () {
-          let wh = $(window).height() - 100;
-          if ($(window).scrollTop() > $('.projectOne').offset().top - wh) {
-              $('.projectOne').addClass('onScrollLeft');
-          }
-          if ($(window).scrollTop() > $('.projectTwo').offset().top - wh) {
-              $('.projectTwo').addClass('onScrollRight');
-          }
-          if ($(window).scrollTop() > $('.projectThree').offset().top - wh) {
-              $('.projectThree').addClass('onScrollLeft');
-          }
-          if ($(window).scrollTop() > $('.projectFour').offset().top - wh) {
-              $('.projectFour').addClass('onScrollRight');
-          }
-      })
+    // scroll down animation for the mouse scroll on the header
+    $('.scrollDown').on('click', function(e){
+    e.preventDefault();
+    $('html').animate({
+        scrollTop: $('#about').offset().top
+    }, 1000);
+    })
 
-  });
-// Adding animation for the images to show on scroll
-;
+    // scroll animation for the projects to appear
+    $(window).scroll(function () {
+        let wh = $(window).height() - 10;
+        if ($(window).scrollTop() > $('.projectOne').offset().top - wh) {
+            $('.projectOne').addClass('onScrollLeft');
+        }
+        if ($(window).scrollTop() > $('.projectTwo').offset().top - wh) {
+            $('.projectTwo').addClass('onScrollRight');
+        }
+        if ($(window).scrollTop() > $('.projectThree').offset().top - wh) {
+            $('.projectThree').addClass('onScrollLeft');
+        }
+        if ($(window).scrollTop() > $('.projectFour').offset().top - wh) {
+            $('.projectFour').addClass('onScrollRight');
+        }
+    })
+
+});
